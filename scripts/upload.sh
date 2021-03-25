f81fd6707214f7502c8b2c533b83369ab7dc7628
Nicolas Primeau <nicolas.primeau@gmail.com>
2021-03-25 18:30:19 -0400

    fix

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -5,6 +5,6 @@ BOOK_TITLE=$1;
 LANGUAGE=$2;
 
 BOOK_PATH=${BASE_PATH}/${BOOK_TITLE}/${LANGUAGE};
-S3_PATH=s3://${S3_BUCKET}${BOOK_TITLE}/${LANGUAGE};
+S3_PATH=s3://${S3_BUCKET}/${BOOK_TITLE}/${LANGUAGE};
 
-aws s3 cp --recursive "${BOOK_PATH}" "${S3_PATH}"
+aws s3 cp --recursive --exclude "audio.mp3" --exclude "video.mp4" "${BOOK_PATH}" "${S3_PATH}"
