a44c30b1a787e500ace4d2ec0d4691c3c74e6350
Nicolas Primeau <nicolas.primeau@gmail.com>
2023-01-31 18:25:14 -0500

    k

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -14,8 +14,6 @@ fi;
 
 echo "Processing audio";
 python ./scripts/process.py -t "${BOOK_TITLE}" -l "${LANGUAGE}" $NO_ASK;
-python ./scripts/process.py -t "${BOOK_TITLE}" -l "${LANGUAGE}" $NO_ASK;
-python ./scripts/process.py -t "${BOOK_TITLE}" -l "${LANGUAGE}" $NO_ASK;
 
 echo "Converting image to png if applicable";
 python ./scripts/convert_image.py -t "${BOOK_TITLE}" -l "${LANGUAGE}";
